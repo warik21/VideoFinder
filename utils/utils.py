2925c3352dc6757b3a12b49d01b8af27ad664bed
Erik Lager <65851326+warik21@users.noreply.github.com>
2024-04-09 20:51:09 +0300

    Added functions to utils
    get_similarity_score - gets the similarity score between two embeddings using cosine similarity
    weighted_embed_text - gives different weights to embeddings.

--- utils/utils.py
+++ utils/utils.py
@@ -10,6 +10,9 @@ from langchain_openai import ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import time
 from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+from typing import Optional
+import torch
 
 
 def get_api_key(api_key_path):
@@ -140,24 +143,39 @@ def add_channel_videos(channel_id, api_key, num_vids=None, df=None):
 
     print(f"Processing {num_vids} videos from channel ID: {channel_id}")
     for video in tqdm.tqdm(videos[:num_vids]):
-        video_id = video['snippet']['resourceId']['videoId']
-        video_url = f"https://www.youtube.com/watch?v={video_id}"
-        video_description_ini = video['snippet']['description']
-        video_transcript_ini = get_video_transcript(video_id)
-
-        # Clean and summarize the description and transcript
-        video_description = clean_description(video_description_ini)
-        video_transcript = clean_transcript(video_transcript_ini)
-
-        video_name = video['snippet']['title']
-        channel_name = video['snippet']['channelTitle']
-        df = df._append({'video_url': video_url,
-                         'video_description': video_description,
-                         'video_transcript': video_transcript,
-                         'video_name': video_name,
-                         'channel_name': channel_name,
-                         }, ignore_index=True)
+        start = time.time()
+        new_row, num_chunks = add_video_details(video)
+        df = df._append(new_row, ignore_index=True)
+        end = time.time()
+        print(f"Processed video in {end - start:.2f} seconds. Transcript split into {num_chunks} chunks.")
     return df
+
+
+def add_video_details(video: dict):
+    """
+    Adds video details to a DataFrame.
+
+    Args:
+        video: A dictionary containing details of a video.
+
+    Returns:
+        The DataFrame containing the video details.
+    """
+    video_id = video['snippet']['resourceId']['videoId']
+    video_url = f"https://www.youtube.com/watch?v={video_id}"
+    video_description = clean_description(video['snippet']['description'])
+    video_transcript, num_chunks = clean_transcript(get_video_transcript(video_id))
+    embedding = weighted_embed_text(video_description, video_transcript)
+
+    # Clean and summarize the description and transcript
+    new_row = {'video_url': video_url,
+               'video_description': video_description,
+               'video_transcript': video_transcript,
+               'video_name': video['snippet']['title'],
+               'channel_name': video['snippet']['channelTitle'],
+               'embedding': embedding
+               }
+    return new_row, num_chunks
 
 
 def clean_description(description):
@@ -225,19 +243,16 @@ def clean_transcript(transcript):
     chat_llm = ChatOpenAI(temperature=0, base_url="http://localhost:1234/v1", api_key="not-needed")
     chain = prompt | chat_llm
 
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=4096, chunk_overlap=512)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=8192, chunk_overlap=512)
     text_chunks = text_splitter.split_text(transcript)
 
     responses = []
     for chunk in text_chunks:
-        time_start = time.time()
         response = chain.invoke({"transcript": chunk})
         clean_response = response.content
         responses.append(clean_response)
-        time_end = time.time()
-        print(f"Chunk processed in {time_end - time_start:.2f} seconds")
 
-    return process_transcript(responses)
+    return process_transcript(responses), len(text_chunks)
 
 
 def process_transcript(responses):
@@ -264,21 +279,89 @@ def process_transcript(responses):
     return " ".join(summary_parts)
 
 
-def get_embedding(text: str, model: "SentenceTransformer") -> list[float]:
+def get_embedding(text: str, model: Optional[SentenceTransformer] = None) -> list[float]:
     """
-    Get the embedding of a text using a SentenceTransformer model.
+    Get the embedding of a text using a SentenceTransformer model. If no model is explicitly provided,
+    the function will initialize and use the default 'sentence-transformers/all-MiniLM-L6-v2' model.
 
     Args:
-        text: The text to get the embedding for.
-        model: The SentenceTransformer model to use for embedding.
+        text (str): The text to get the embedding for.
+        model (Optional[SentenceTransformer], optional): The SentenceTransformer model to use for embedding.
+            If not specified, the function initializes the 'all-MiniLM-L6-v2' model by default.
 
     Returns:
-        The embedding of the text as a list of floats.
+        list[float]: The embedding of the text as a list of floats.
+
+    Example:
+        # Without specifying a model; uses the default model
+        embedding = get_embedding("This is a sample text.")
+        print(embedding)
+
+        # With a specified model
+        custom_model = SentenceTransformer('your-custom-model-name')
+        embedding = get_embedding("This is a sample text.", model=custom_model)
+        print(embedding)
     """
+    if model is None:
+        model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
     if not text.strip():
         print("Attempted to get embedding for empty text.")
         return []
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    model.to(device)
 
-    embedding = model.encode(text)
+    return model.encode(text)
 
-    return embedding.tolist()
+
+def get_similarity_score(embedding1: list[float], embedding2: list[float]) -> float:
+    """
+    Get the similarity score between two embeddings using cosine similarity.
+
+    Args:
+        embedding1: The first embedding.
+        embedding2: The second embedding.
+
+    Returns:
+        The similarity score between the two embeddings.
+    """
+    if not embedding1 or not embedding2:
+        print("Attempted to get similarity score for empty embeddings.")
+        return 0.0
+
+    similarity_score = cosine_similarity([embedding1], [embedding2])[0][0]
+
+    return similarity_score
+
+
+def weighted_embed_text(transcript: str, description: str, weight_transcript: float = 0.7,
+                        weight_description: float = 0.3) -> list[float]:
+    """
+    Generate a weighted embedding for a video by separately embedding its transcript and description, then combining these embeddings using specified weights.
+
+    This function allows for the emphasis of certain parts of the video's content over others in the resulting embedding. By adjusting the weights for the transcript and the description, users can tailor the embedding to better suit their needs for comparing video content to search queries or other texts.
+
+    Args:
+        transcript (str): The transcript of the video to be embedded.
+        description (str): The description of the video to be embedded.
+        weight_transcript (float, optional): The weight to be applied to the transcript's embedding. Defaults to 0.7.
+        weight_description (float, optional): The weight to be applied to the description's embedding. Defaults to 0.3.
+
+    Returns:
+        list[float]: The weighted embedding of the video's transcript and description as a list of floats. This embedding represents a single vector that combines the information from both the transcript and the description, adjusted by their respective weights.
+
+    Note:
+        The function assumes the existence of a previously defined `embed_text` function that takes a string as input and returns its embedding as a list of floats. It is also assumed that the embeddings generated by `embed_text` are of a fixed size and directly comparable (i.e., they are produced by the same model or process).
+
+    Example:
+        # Example usage for a video with a specific transcript and description
+        transcript = "Here is the video transcript."
+        description = "Here is the video description."
+        weighted_embedding = weighted_embed_text(transcript, description, 0.7, 0.3)
+        print(weighted_embedding)  # This will print the weighted embedding vector as a list of floats.
+    """
+    transcript_embedding = get_embedding(transcript)
+    description_embedding = get_embedding(description)
+
+    # Applying weights
+    weighted_embedding = (transcript_embedding * weight_transcript) + (description_embedding * weight_description)
+    return weighted_embedding.tolist()
