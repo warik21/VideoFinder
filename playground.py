6b7030e183eee1dd92d09c7b55aef19f35c4a9bc
Erik Lager <65851326+warik21@users.noreply.github.com>
2024-04-09 20:50:05 +0300

    added some games in playground which let me find the best fit.

--- playground.py
+++ playground.py
@@ -1,23 +1,33 @@
 from utils.utils import *
 import os
 import pandas as pd
+import time
+import numpy as np
+from ast import literal_eval
 
 
-api_key = get_api_key(os.path.join('..', 'keys', 'VideoFinder', 'YouTubeAPIKey.txt'))
+df = pd.read_csv('videos_df3.csv')
+# subset first 5 videos
+df = df.head(5)
 
-# Example Usage
-# channels = ["https://www.youtube.com/@juliensimonfr", "https://www.youtube.com/@sentdex"]
-channels = ["https://www.youtube.com/@juliensimonfr"]
-df = pd.DataFrame()
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
-for channel in channels:
-    channel_id, channel_name = extract_channel_id_and_name(download_html(channel))
-    print(f"Processing channel: {channel_name}")
-    df = add_channel_videos(channel_id, api_key, num_vids=5, df=df)
+# initiate a text prompt to compare to the embeddings of the videos
+inp = input("What do you want to watch?")
 
-description = df['video_description'][0]
-transcript = df['video_transcript'][0]
-clean_description = clean_description(description)
-print(clean_description)
-clean_transcript = clean_transcript(transcript)
-print(clean_transcript)
+# encode the input
+start_time = time.time()
+inp_embedding = get_embedding(inp).reshape(1, -1)
+end_time = time.time()
+execution_time = end_time - start_time
+print(f"The execution time of get_embedding function is: {execution_time} seconds")
+
+# create a search object
+existing_embeddings = df['embedding'].apply(literal_eval)
+existing_embeddings = [np.asarray(existing).reshape(1, -1) for existing in existing_embeddings]
+
+similarities = [cosine_similarity(existing_embedding, inp_embedding) for existing_embedding in existing_embeddings]
+
+closest_video = df.iloc[np.argmax(similarities)]
+
+print(f"Closest video to your input is: {closest_video['video_name']}, Here is a link: {closest_video['video_url']}")
