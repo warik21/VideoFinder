abed67184b571e24a60b7938b6d14c62c24fba60
Erik Lager <65851326+warik21@users.noreply.github.com>
2024-04-13 17:37:19 +0300

    Started applying recursive learning

--- playground.py
+++ playground.py
@@ -1,26 +1,49 @@
 from utils.utils import *
-import os
 import pandas as pd
 import time
+import os
 import numpy as np
 from ast import literal_eval
+import torch
+from transformers import DistilBertModel, DistilBertTokenizer
+from utils.utils import clean_transcript
+
+df = pd.read_csv('videos_df.csv')
+
+# model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased')
+model = DistilBertModel.from_pretrained('distilbert-base-uncased')
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+for i in range(len(df.index)):
+    video_name = df.iloc[i]['video_name']
+    video_description = df.iloc[i]['video_description']
+    video_transcript = df.iloc[i]['video_transcript']
+
+    # Make these include embedding everything
+    embedding_bert = get_joint_embedding_bert(video_transcript)
+    mean_embedding_bert = get_mean_embedding(video_transcript)
+    embedding_hf = get_embedding(video_transcript)
 
 
-df = pd.read_csv('videos_df3.csv')
-# subset first 5 videos
-df = df.head(5)
 
-model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
-# initiate a text prompt to compare to the embeddings of the videos
-inp = input("What do you want to watch?")
 
-# encode the input
-start_time = time.time()
-inp_embedding = get_embedding(inp).reshape(1, -1)
-end_time = time.time()
-execution_time = end_time - start_time
-print(f"The execution time of get_embedding function is: {execution_time} seconds")
+for transcript in test_transcripts:
+    start = time.time()
+    mean_embedding = get_mean_embedding(text=transcript, model=model,
+                                        tokenizer=tokenizer, device=device)
+    print(f"Time taken to get mean embedding: {time.time() - start}")
+
+for transcript in test_transcripts:
+    start = time.time()
+    # clean_transcript = clean_transcript(transcript)
+    embedding = get_embedding(transcript)
+    print(f"Time taken to clean transcript: {time.time() - start}")
+
+
+accuracy = correct_matches / total_prompts
+print(f"Accuracy: {accuracy * 100:.2f}%")
 
 # create a search object
 existing_embeddings = df['embedding'].apply(literal_eval)
