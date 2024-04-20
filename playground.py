6c0ad0dd588c68c094373e2ec84df4bb0abce7b2
Erik Lager <65851326+warik21@users.noreply.github.com>
2024-04-20 18:22:57 +0300

    Changed playground for different outputs

--- playground.py
+++ playground.py
@@ -6,17 +6,4 @@ import numpy as np
 from Generation.generate_dfs import *
 import os
 
-df = pd.read_csv('videos_df.csv')
-# subset for testing
-df = df.head(5)
-
-# Generate embeddings for each video
-video_embeddings_bert, video_embeddings_mean_bert, video_embeddings_hf, video_names = generate_embeddings_df(df, 'embeddings2.csv')
-
-# define similarities df:
-sm_df, correct_matches_dict = generate_similarities_df(df, 'similarities2.csv', video_embeddings_bert,
-                                                       video_embeddings_mean_bert, video_embeddings_hf, video_names)
-
-print(f"Accuracy Bert: {correct_matches_dict['bert'] / len(df) * 100:.2f}%")
-print(f"Accuracy HF: {correct_matches_dict['hf'] / len(df) * 100:.2f}%")
-print(f"Accuracy Mean Bert: {correct_matches_dict['mean_bert'] / len(df) * 100:.2f}%")
+df = generate_df(channels=['https://www.youtube.com/@TwoMinutePapers'], path='csvs/testing_df.csv', num_videos=10)
